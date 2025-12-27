a132c01f1b9a023567a58b100bc9a96d87b37c42
anjalikasingh2003 <Anjalika Singh>
2025-12-27 21:31:51 +0530

    test

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,6 +1,6 @@
 from src.my_test import divide
 from src.calc import divide_l
 def test_divide_by_zero():
-    print("testing")
+    print("testingn")
     a= divide(10, 2) == 5
     b= divide_l(10, 2) == 5
