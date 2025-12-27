37090106776090b3770502d69fe1497c78ba66c8
anjalikasingh2003 <Anjalika Singh>
2025-12-27 23:06:55 +0530

    Test cloud

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,6 +1,6 @@
 from src.my_test import divide
 from src.calc import divide_l
 def test_divide_by_zero():
-    print("testingn mm fdk,n")
+    print("testingn mm fdk,nsmd")
     a= divide(10, 2) == 5
     b= divide_l(10, 2) == 5
