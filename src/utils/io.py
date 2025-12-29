68e9493f4341f2f5b0c5513c6faf59e3a22cd841
anjalikasingh2003 <Anjalika Singh>
2025-12-29 21:10:21 +0530

    Test cloud

--- src/utils/io.py
+++ src/utils/io.py
@@ -1,2 +1,2 @@
 def read_file(p):
-    open("missingkjmkkkkbfh-tisha-ishu---csf-yed,kxfeddecdsfdfefkikvml,7777ohujmle.txt").read()
+    open("missingkjmkkkkbfh-tisha-ishu---kvml,7777ohujmle.txt").read()
