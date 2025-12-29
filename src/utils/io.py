9c95eedbb67a99c068c5f9a6a6512a14d13111aa
anjalikasingh2003 <Anjalika Singh>
2025-12-29 17:37:29 +0530

    AI: Fix CI failure

--- src/utils/io.py
+++ src/utils/io.py
@@ -1,2 +1,3 @@
 def read_file(p):
-    open("missingkjmkkkkbfh-tisha-ishu-nkj--csf-yefedfedxfeddecsef,d-ewficdfsfdkikvml,7777ohujmle.txt").read()
+    with open(p, 'r') as f:
+        return f.read()
